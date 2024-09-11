a863b27d4d07ff50f00b6d72e582a690fd9be43d
T.D Rasindu Dulshan Siriwardhana <dulshansiriwardhana70@gmail.com>
2024-09-11 21:46:36 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App.jsx";
 import "./index.css";
 import "./assets/fonts/fonts.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")).render(<App />);
